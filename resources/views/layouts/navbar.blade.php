cd05732f17f19b72dcdc2b902b08af7c2b137ac9
imannueladitya <imannuelkevin1@gmail.com>
2023-06-13 10:50:28 +0700

    Web 1.0

---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,20 @@
+<nav class="navbar navbar-expand-lg navbar" style="background-color: rgb(91,186,174);">
+  <a class="navbar-brand"style="color: rgb(255,230,167)" href="#">Admin AJI</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link"style="color: rgb(255,230,167)" href="/">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" style="color: rgb(255,230,167)"href="/stocklist">Stock List</a>
+      </li>
+      <li class="nav_item">
+      <a class="nav-link"style="color: rgb(255,230,167);" href="/register">Register</a>
+      </li> 
+    </ul>
+  </div>
+</nav>
